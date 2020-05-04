963068aa3899090f1fff372003383edc81e8842b
Seo, Yujin <tjdbwls8998@naver.com>
2020-05-04 09:59:47 +0900

    중간고사

--- skhu-syj/public/mid-exam/mid4.js
+++ skhu-syj/public/mid-exam/mid4.js
@@ -1,4 +1,15 @@
 
 function sum (...a) {
-let result = 0;
+    let result = 0;
+    for(let i = 0; i < a.length; ++i)
+       result += a[i];
+    return result;   
 }
+
+
+function test_sum (f){
+
+    a[i] = Math.floor(Math.random() * (10 - 0 + 5) );
+}
+
+console.log(a[i] = Math.floor(Math.random() * (10 - 0 + 5) ));
---
+++ skhu-syj/public/mid-exam/mid5.js
@@ -0,0 +1,11 @@
+
+let a = [];
+
+for(let i = 0; i < 100; i++){
+    a[i] = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+}
+
+for(let i = 0; i < a.length; i++){
+    if(a[i] % 2 == 0)
+        
+}
